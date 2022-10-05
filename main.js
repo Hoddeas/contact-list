9363a1f6036103e24c1d9a64ee7214baa821102d
Hoddeas <fakehoddeas@gmail.com>
2022-10-05 13:28:22 -0600

    s

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let outputEl = document.getElementById('output');
 
+// Global Variable
+let contacts = loadContacts();
+displayAllContacts();
+
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
 
@@ -13,7 +17,7 @@ function goBtnHandler() {
   let selection = menuEl.value;
 
   if (selection === 'display-all') {
-    displayContacts();
+    displayAllContacts();
   } else if (selection === 'add') {
     addContact();
   } else if (selection === 'remove') {
@@ -26,22 +30,82 @@ function goBtnHandler() {
 }
 
 // MENU FUNCTIONS
-function displayContacts() {
-  console.log('Display Contacts');
+
+// Display all contacts
+function displayAllContacts() {
+  let outputStr = '';
+  for (let i = 0; i < contacts.length; i++) {
+    outputStr += getTaskHTMLStr(contacts[i], i);
+  }
+  outputEl.innerHTML = outputStr;
 }
 
 function addContact() {
-  console.log('Add Contact');
+  let name = prompt("Enter Contact Name:");
+  let email = prompt("Enter Contact Email:"); 
+  let phone = prompt("Enter Contact Phone:");
+  let country = prompt("Enter Contact Country:");
+  contacts.push(newContact(name, email, phone, country));
+  saveContacts();
+  outputEl.innerHTML = `New Contact Added (${name})`
 }
 
 function removeContact() {
-  console.log('Remove Contact');
+  let index = +prompt("Enter Contact Number:");
+  if (index >= 0 && index < contacts.length) {
+    contacts.splice(index, 1);
+    saveContacts();
+    displayAllContacts();
+  } else {
+    outputEl.innerHTML = '<p style="color:red;"> Invalid Contact Number </p>';
+  }
 }
 
 function displayByName() {
-  console.log('Display by Name');
+  let searchName = prompt("Enter Contact Name:")
+  let outputName = "";
+  for (i = 0; i < contacts.length; i++) {
+    if (contacts[i].name == searchName) {
+      outputName += getTaskHTMLStr(contacts[i], i);
+    }
+    outputEl.innerHTML = outputName;
+  }
 }
 
 function displayByCountry() {
   console.log('Display by Country');
+}
+
+// HELPER FUNCTIONS
+
+// Return New Contact
+function newContact(contactName, contactEmail, contactPhone, contactCountry) {
+  return {
+    name: contactName,
+    email: contactEmail,
+    phone: contactPhone,
+    country: contactCountry
+  };
+}
+
+// Get HTML for given task
+function getTaskHTMLStr(contact, i) {
+  return `
+    <div>
+      <h3>${i}: ${contact.name}</h3>
+      ${contact.email} <br>
+      ${contact.phone} (${contact.country})
+    </div>
+  `
+}
+
+// Save tasks to localStorage
+function saveContacts() {
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+} 
+
+// Load tasks from localStorage
+function loadContacts() {
+  let contactsStr = localStorage.getItem("contacts");
+  return JSON.parse(contactsStr) ?? [];
 }
